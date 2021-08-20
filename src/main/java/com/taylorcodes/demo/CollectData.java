ab167398f47bc6c4b8285a8cc7c14c7dce3d90c4
taylor-codes <66972156+taylor-codes@users.noreply.github.com>
2021-08-19 21:54:55 -0500

    Feature: Produce to Kafka (#2)
    * Basic Kafka Producer working.
    
    * Updated pom.xml java version to 16.
    
    * Adding data.csv.
    
    * Clean up.

--- src/main/java/com/taylorcodes/demo/CollectData.java
+++ src/main/java/com/taylorcodes/demo/CollectData.java
@@ -8,6 +8,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 public class CollectData {
     public static void main(String[] args) throws IOException {
         System.out.print("Hello World!\n\n");
@@ -17,14 +22,13 @@ public class CollectData {
 
     public static void readCSV() throws IOException {
 
-        // https://www.baeldung.com/opencsv
-        // List<List<String>> records = new ArrayList<List<String>>();
-        // try (CSVReader csvReader = new CSVReader(new FileReader("book.csv"));) {
-        //     String[] values = null;
-        //     while ((values = csvReader.readNext()) != null) {
-        //         records.add(Arrays.asList(values));
-        //     }
-        // }
+        Properties props = new Properties();
+
+        props.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
 
         List<List<String>> records = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/data.csv"))) {
@@ -33,8 +37,11 @@ public class CollectData {
                 String[] values = line.split(",");
                 records.add(Arrays.asList(values));
                 System.out.println(line);
+                producer.send(new ProducerRecord<String,String>("DATA-COLLECTION", line, line));
             }// end while()
             System.out.print("\n" + records);
+
+            producer.close();
         }
 
         System.exit(0);
