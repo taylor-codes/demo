7e06d2260140902fbc28cd9c4d955187a637c18d
Taylor Conkle <t.conkle01@gmail.com>
2021-08-19 21:47:14 -0500

    Basic Kafka Producer working.

--- src/main/java/com/taylorcodes/demo/CollectData.java
+++ src/main/java/com/taylorcodes/demo/CollectData.java
@@ -8,6 +8,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 public class CollectData {
     public static void main(String[] args) throws IOException {
         System.out.print("Hello World!\n\n");
@@ -26,6 +31,14 @@ public class CollectData {
         //     }
         // }
 
+        Properties props = new Properties();
+
+        props.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
         List<List<String>> records = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/data.csv"))) {
             String line;
@@ -33,8 +46,11 @@ public class CollectData {
                 String[] values = line.split(",");
                 records.add(Arrays.asList(values));
                 System.out.println(line);
+                producer.send(new ProducerRecord<String,String>("DATA-COLLECTION", line, line));
             }// end while()
             System.out.print("\n" + records);
+
+            producer.close();
         }
 
         System.exit(0);
